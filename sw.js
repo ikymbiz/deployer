f4588393d2f933c87f10238b7de78b11a8166e34
ikymbiz <68386130+ikymbiz@users.noreply.github.com>
2026-05-02 20:02:32 +0900

    Update via MyPages Hub Deployer

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'mypages-hub-admax-fixed-v2';
-const ASSETS = ['./', './index.html'];
+const CACHE_NAME = 'mypages-hub-manual-gemini-pwa-v1';
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg', './icon-192.png', './icon-512.png', './maskable-512.png'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).catch(() => null));
@@ -7,9 +7,7 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null)))
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null))));
   self.clients.claim();
 });
 
