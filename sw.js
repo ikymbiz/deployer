7a6e1ee8c62614401d1ec3d721d54add3737d778
ikymbiz <68386130+ikymbiz@users.noreply.github.com>
2026-05-02 19:51:39 +0900

    Update via MyPages Hub Deployer

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mypages-hub-integrated-v1';
+const CACHE_NAME = 'mypages-hub-admax-fixed-v2';
 const ASSETS = ['./', './index.html'];
 
 self.addEventListener('install', event => {
